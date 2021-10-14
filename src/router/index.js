5741958954b998d3d0864a7903db96b87c688743
taka198729 <taka198729@gmail.com>
2021-10-15 01:12:46 +0900

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import Home from '../views/Home.vue'
+import UserList from '../views/UserList.vue'
+import Chatboard from '../views/Chatboard.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'UserList',
+    component: UserList
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/chat',
+    name: 'Chatboard',
+    component: Chatboard
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
